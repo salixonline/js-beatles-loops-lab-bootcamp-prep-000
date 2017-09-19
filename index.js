1b561a3aecd56b2348d5430bacf1fcc50487ced3
Armando <salixonline@gmail.com>
2017-09-19 08:41:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 function theBeatlesPlay(musicians, instruments) {
   var newArray = [];
-
   for (var i = 0; i < musicians.length; i++) {
     newArray.push(`${musicians[i]} plays ${instruments[i]}`);
   }
-
   return newArray;
 }
 
@@ -15,4 +13,12 @@ function johnLennonFacts(array) {
     i++;
   }
   return array;
+}
+
+function iLoveTheBeatles(n) {
+  var newArray = [];
+  do {
+    newArray.push("I love the Beatles!");
+    n++;
+  } while (n < 15);
 }
