6b32c9aeb97e861039ce2f98dce4f8c71ec8e5c7
Armando <salixonline@gmail.com>
2017-09-19 08:38:19 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,18 @@
+function theBeatlesPlay(musicians, instruments) {
+  var newArray = [];
+
+  for (var i = 0; i < musicians.length; i++) {
+    newArray.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+
+  return newArray;
+}
+
+function johnLennonFacts(array) {
+  var i = 0;
+  while (i < array.length) {
+    array[i] = `${array[i]}!!!`
+    i++;
+  }
+  return array;
+}
